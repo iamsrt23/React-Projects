88e29939d8199ee5918800e7d9c1c632f74f2630
Mr.Bee <bumblebee.programming251@gmail.com>
2024-08-22 14:57:43 +0530

    React-page

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar =()=>{
+    return(
+        <div>
+            <nav>
+                <img src="react-logo.png" alt="react-logo" width="60px" />
+                <h2>ReactFacts</h2>
+                <h3>React Course - Project 1</h3>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
